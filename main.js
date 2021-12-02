a255a8b673d131901c8c9ff5af207f1baafbc38d
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-12-02 15:59:51 +0100

    fixing the login and loader

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-//loader animation
-const loader = document.querySelector(".loader");
+"use stict;";
 
-
-
-function init() {
-    setTimeout(() => {
-      loader.style.opacity = 0;
-      main.style.display = "block";
-      setTimeout(() => (main.style.opacity = 1), 50);
-    }, 3000);
-  }
-  init();
-
-  //end of loader animaion
-  
-  //form
+//form
 
 const form = document.querySelector(".login-form");
 const username = document.getElementById("Username");
@@ -24,48 +10,74 @@ const messages = document.querySelectorAll(".message");
 const button = document.querySelector(".form-submit-btn");
 
 const error = (input, message) => {
-    input.nextElementSibling.classList.add("error");
-    input.nextElementSibling.textContent = message;
+  input.nextElementSibling.classList.add("error");
+  input.nextElementSibling.textContent = message;
 };
 
 const succes = (input) => {
-    input.nextElementSibling.classList.remove("error");
+  input.nextElementSibling.classList.remove("error");
 };
 const checkRequiredFields = (inputArr) => {
-    inputArr.forEach((input) => {
-      if (input.value.trim() === "") {
-          error(input, `${input.id} is required`);
-        }
-    });
+  inputArr.forEach((input) => {
+    if (input.value.trim() === "") {
+      error(input, `${input.id} is required`);
+    }
+  });
 };
 const checkLength = (input, min) => {
-    if (input.value.trim().length < min) {
-        error(input, `${input.id} must be at least ${min} characters`);
-    } else {
-        succes(input);
-    }
+  if (input.value.trim().length < min) {
+    error(input, `${input.id} must be at least ${min} characters`);
+  } else {
+    succes(input);
+  }
 };
 
-
+const checkName = (input) => {
+  if (input.value === "Jonas") {
+    succes(input);
+    init();
+    // window.location.href="index.html";
+  } else if (input.value === "Peter") {
+    succes(input);
+    init();
+    // window.location.href="index.html";
+  } else if (input.value === "Klaus") {
+    succes(input);
+    init();
+    // window.location.href="index.html";
+  } else if (input.value === "Dannie") {
+    succes(input);
+    init();
+    // window.location.href="index.html";
+  } else {
+    error(input, `${input.id} is wrong`);
+  }
+};
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    checkLength(username, 2);
-    checkLength(password, 8);
-    checkRequiredFields([username, password]);
-    handleButton();
-    
+  e.preventDefault();
+  checkName(username);
+  checkLength(password, 8);
+  checkRequiredFields([username, password]);
 });
 
-
-
-button.addEventListener("click", handleButton);
-
-function handleButton(){
-    location.href="index.html";
-    const nameBartender = document.querySelector(".name");
-    const nameValue = document.getElementById("Username").value;
-    nameBartender.innerHTML = nameValue;
-}
 // end of form
 
+//loading animation
+
+function init() {
+  const nameBartender = document.querySelector(".name");
+  const nameValue = document.getElementById("Username").value;
+  nameBartender.innerHTML = nameValue;
+  const screen1 = document.querySelector(".screen1");
+  const loader = document.querySelector(".loader");
+  const main = document.querySelector(".main");
+  screen1.style.display = "none";
+  loader.style.display = "flex";
+  loader.style.visibility = "visible";
+  setTimeout(() => {
+    loader.style.display = "none";
+    loader.style.visibility = "hidden";
+    main.style.display = "block";
+  }, 3000);
+}
