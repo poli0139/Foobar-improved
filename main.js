340918a860b46a4fb7779f35b5cafa9fc0eec6c2
Polina <p.artamonova@gmail.com>
2021-12-03 16:12:44 +0100

    darkmode partly done

--- main.js
+++ main.js
@@ -1,87 +1,24 @@
-"use stict;";
+//DARK MODE
+let darkmode = false;
 
-//form
+const modeSwitch = document.querySelector(".switch");
 
-const form = document.querySelector(".login-form");
-const username = document.getElementById("Username");
-const password = document.getElementById("Password");
-const message = document.getElementById("message");
-const messages = document.querySelectorAll(".message");
-const button = document.querySelector(".form-submit-btn");
-
-const error = (input, message) => {
-  input.nextElementSibling.classList.add("error");
-  input.nextElementSibling.textContent = message;
-};
-
-const succes = (input) => {
-  input.nextElementSibling.classList.remove("error");
-};
-const checkRequiredFields = (inputArr) => {
-  inputArr.forEach((input) => {
-    if (input.value.trim() === "") {
-      error(input, `${input.id} is required`);
-    }
-  });
-};
-const checkLength = (input, min) => {
-  if (input.value.trim().length < min) {
-    error(input, `${input.id} must be at least ${min} characters`);
+modeSwitch.onclick = function () {
+  if (darkmode == false) {
+    darkmode = true;
+    console.log(darkmode);
+    document.querySelector("body").classList.add("dark");
+    document.querySelector(".bartender-img").src =
+      "assets/bartender-yellow.png";
+    document.querySelector(".switch").src = "assets/FooBar-switch2.png";
+    document.querySelector(".logo").src = "assets/logo-yellow.png";
+    console.log(document.querySelectorAll(".next-icon"));
+    document.querySelectorAll(".next-icon").src = "assets/next_yellow.png";
   } else {
-    succes(input);
+    darkmode = false;
+    document.querySelector("body").classList.remove("dark");
+    document.querySelector(".bartender-img").src = "assets/bartender-green.png";
+    document.querySelector(".switch").src = "assets/FooBar-switch.png";
+    document.querySelector(".logo").src = "assets/logo-green.png";
   }
 };
-
-const checkName = (input) => {
-  if (input.value === "Jonas") {
-    2;
-    succes(input);
-    init();
-    // window.location.href="index.html";
-  } else if (input.value === "Peter") {
-    succes(input);
-    init();
-    // window.location.href="index.html";
-  } else if (input.value === "Klaus") {
-    succes(input);
-    init();
-    // window.location.href="index.html";
-  } else if (input.value === "Dannie") {
-    succes(input);
-    init();
-    // window.location.href="index.html";
-  } else {
-    error(input, `${input.id} is wrong`);
-  }
-};
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  checkName(username);
-  checkLength(password, 8);
-  checkRequiredFields([username, password]);
-});
-
-// end of form
-
-//loading animation
-
-function init() {
-  const nameBartender = document.querySelector(".name");
-  const nameValue = document.getElementById("Username").value;
-  nameBartender.innerHTML = nameValue.trim();
-  const screen1 = document.querySelector(".screen1");
-  const loader = document.querySelector(".loader");
-  const main = document.querySelector(".main");
-  const logo2 = document.querySelector(".logo2");
-  screen1.style.display = "none";
-  loader.style.display = "flex";
-  loader.style.visibility = "visible";
-
-  setTimeout(() => {
-    loader.style.display = "none";
-    loader.style.visibility = "hidden";
-    main.style.display = "grid";
-  }, 3000);
-}
-//enf of loading animation
