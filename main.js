245578b65124714d0270c7fcecd2a20fe09455cf
Adelina Radulescu <adel0896@stud.kea.dk>
2021-12-06 13:31:05 +0100

    bubbles-finished

--- main.js
+++ main.js
@@ -85,3 +85,64 @@ function init() {
   }, 3000);
 }
 //enf of loading animation
+
+//animation for taps
+function createBubbles(bubblesNumber, percent) {
+  for (let i = 0; i < bubblesNumber; i++) {
+    const random = Math.floor(Math.random() * ((percent * 200) / 100));
+    const random2 = Math.floor(Math.random() * ((percent * 300) / 100));
+    const random3 = Math.floor(Math.random() * ((percent * 150) / 100));
+    const random4 = Math.floor(Math.random() * ((percent * 160) / 100));
+    const random5 = Math.floor(Math.random() * ((percent * 160) / 80));
+    const random6 = Math.floor(Math.random() * ((percent * 290) / 80));
+    const random7 = Math.floor(Math.random() * ((percent * 490) / 75));
+
+    const bubble1 = document.createElement("div");
+    bubble1.className = "bubble1";
+    bubble1.style.left = random + "px";
+    bubble1.style.opacity = random + "%";
+    bubble1.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles1").appendChild(bubble1);
+    const bubble2 = document.createElement("div");
+    bubble2.className = "bubble2";
+    bubble2.style.left = random2 + "px";
+    bubble2.style.opacity = random2 + "%";
+    bubble2.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles2").appendChild(bubble2);
+    const bubble3 = document.createElement("div");
+    bubble3.className = "bubble3";
+    bubble3.style.left = random3 + "px";
+    bubble3.style.opacity = random3 + "%";
+    bubble3.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles3").appendChild(bubble3);
+    const bubble4 = document.createElement("div");
+    bubble4.className = "bubble4";
+    bubble4.style.left = random4 + "px";
+    bubble4.style.opacity = random4 + "%";
+    bubble4.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles4").appendChild(bubble4);
+    const bubble5 = document.createElement("div");
+    bubble5.className = "bubble5";
+    bubble5.style.left = random5 + "px";
+    bubble5.style.opacity = random5 + "%";
+    bubble5.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles5").appendChild(bubble5);
+    const bubble6 = document.createElement("div");
+    bubble6.className = "bubble6";
+    bubble6.style.left = random6 + "px";
+    bubble6.style.opacity = random6 + "%";
+    bubble6.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles6").appendChild(bubble6);
+    const bubble7 = document.createElement("div");
+    bubble7.className = "bubble7";
+    bubble7.style.left = random7 + "px";
+    bubble7.style.opacity = random7 + "%";
+    bubble7.style.animationDelay = Math.random() * 2 + "s";
+    document.querySelector(".bubbles7").appendChild(bubble7);
+  }
+}
+// let bubbles = document.querySelector(".bubbles");
+
+window.addEventListener("DOMContentLoaded", () => {
+  createBubbles(60, 100);
+});
