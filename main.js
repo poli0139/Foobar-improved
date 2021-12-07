a48fbda61f958b2890351eba95225b4e0f0dd699
Maria-Lavinia Otelea <otelea.lavi@gmail.com>
2021-12-07 01:41:18 +0100

    changes

--- main.js
+++ main.js
@@ -171,14 +171,16 @@ form.addEventListener("submit", (e) => {
 function init() {
   const nameBartender = document.querySelector(".name");
   const nameValue = document.getElementById("Username").value;
-  nameBartender.innerHTML = nameValue.trim();
+  nameBartender.innerHTML = nameValue;
   const screen1 = document.querySelector(".screen1");
   const loader = document.querySelector(".loader");
   const main = document.querySelector(".main");
   const header = document.querySelector("header");
   const body = document.querySelector("body");
+  const logo2 = document.querySelector(".logo2 img");
   screen1.style.display = "none";
-
+logo2.style.display = "flex";
+logo2.style.visibility="visible"
   loader.style.display = "flex";
   loader.style.visibility = "visible";
   body.style.borderLeft = "0";
